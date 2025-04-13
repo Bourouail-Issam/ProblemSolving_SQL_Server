ac0f9c8f9e97e9d2dc42cda660284514b3956cf0
Bourouail Issam <issambourouail33@gmail.com>
2025-04-13 05:46:24 +0200

    Add queries to get vehicle counts per make (1950–2000) and filter makes with over 12k vehicles
    - Query to count vehicles made between 1950 and 2000 per make, ordered by number of vehicles descending.
    - Query to retrieve makes with more than 12,000 vehicles manufactured between 1950 and 2000 using HAVING.
    - Alternative version of the same filter without using HAVING clause.

--- Problems SQL Server.sql
+++ Problems SQL Server.sql
@@ -1,3 +1,42 @@
---   Problem 3 : Get number vehicles made between 1950 and 2000
+-- Problem 3 : Get number vehicles made between 1950 and 2000
 select count(*) as NumberOfVehicles from VehicleDetails
 where   Year between 1950 and 2000 
+
+-- Problem 4 : Get number vehicles made between 1950 and 2000 per make and order them by Number Of Vehicles Descending
+
+select * from Makes;
+select * from VehicleDetails;
+
+select Makes.Make, COUNT(*) as NumberVehicles
+
+from VehicleDetails inner join Makes
+
+      on VehicleDetails.MakeID = Makes.MakeID
+where (VehicleDetails.Year between 1950 and 2000)
+group by Makes.Make
+order by NumberVehicles desc;
+
+-- Problem 5 : Get All Makes that have manufactured more than 12000 Vehicles in years 1950 to 2000
+
+select Makes.Make, COUNT(*) as NumberVehicles
+
+from VehicleDetails inner join Makes
+
+      on VehicleDetails.MakeID = Makes.MakeID
+where (VehicleDetails.Year between 1950 and 2000)
+group by Makes.Make
+having count(*) > 12000
+order by NumberVehicles desc;
+
+-- without using having 
+select * from (
+select Makes.Make, COUNT(*) as NumberVehicles
+
+from VehicleDetails inner join Makes
+
+      on VehicleDetails.MakeID = Makes.MakeID
+where (VehicleDetails.Year between 1950 and 2000)
+group by Makes.Make
+) R1
+where NumberVehicles>12000
+ORDER BY NumberVehicles desc
